89775fede5425664e5c14070d20511079ad61c14
陈林波 <jinjinanan@hotmail.com>
2018-09-15 11:23:23 +0800

    堆和哈夫曼树定义

--- 数据结构&算法/heap.c
+++ 数据结构&算法/heap.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 typedef struct HeapStruct *MaxHeap;
 struct HeapStruct{
@@ -14,7 +15,7 @@ MaxHeap Create(int MaxSize){
     H->data = malloc((MaxSize+1)*sizeof(int)); // 堆从下表1的地方开始存放   动态的申请数组
     H->size = 0;
     H->Capacity = MaxSize;
-    H->data[0] = MaxSize;  //H->data 是 int 指针 。 这里data做数组使用 data[0] 直接存储？
+    H->data[0] = UINT_MAX;  //H->data 是 int 指针 。 可以忽略data的类型当作数组来用
     return H;
 }
 
@@ -26,19 +27,61 @@ int IsFull(MaxHeap H){
     return 0;
 }
 
+// 判断堆是否满了
+int IsEmpty(MaxHeap H){
+    if(H->size == 0){
+        return 1;
+    }
+    return 0;
+}
+
 // 插入
 void Insert(MaxHeap H, int item){
     int i;
+    if(IsFull(H)){
+        printf("最大堆已满");
+        return;
+    }
+    i = ++H->size;
+    for(;H->data[i/2]<item;i/=2){  //哨兵
+        H->data[i] = H->data[i/2];
+    }
+    H->data[i] = item;
     
 }
 
+// 删除
+int Delete(MaxHeap H,int item){
+    int Parent,Child;
+    int MaxItem,temp;
+    if(IsEmpty(H)){
+        printf("最大堆以为空");
+        return -1;
+    }
+    MaxItem = H->data[1];
+    temp = H->data[H->size--];
+    for(Parent =1;Parent*2<=H->size;Parent=Child){//Parent*2<=H->size 有子节点
+        Child = Parent *2;
+        if((Child != H->size)&&(H->data[Child] < H->data[Child+1])){
+            Child++;
+        }
+        if(temp >= H->data[Child]) {
+            break;
+        }
+        else{
+            H->data[Parent] = H->data[Child];
+        }
+    }
+    H->data[Parent] = temp;
+    return MaxItem;
+}
+
+// 最大堆建立
+// 将N个元素按顺序存入，先买足完全二叉树的结构特性
+// 调整各个结点为止，满足最大二叉树的有序性。
+
 int main(int argc, char const *argv[])
 {
-    int i = 4;
-    MaxHeap h = malloc(sizeof(struct HeapStruct));
-    h->data = malloc(sizeof(int));
-    h->data[0] = i;
-    h->data[1] = 2;
-    printf("%p", h->data[1]);
+   
     return 0;
 }
